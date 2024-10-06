c42230080be0daebfc5c3880272792fa615a3ecc
Tanis Pérez Nartallo <tanisperez@gmail.com>
2024-10-06 18:10:28 +0200

    Add Spring Boot skeleton files from Spring Initialzr

---
+++ src/main/java/codes/tanis/sensapi/SensapiApplication.java
@@ -0,0 +1,13 @@
+package codes.tanis.sensapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codes/tanis/sensapi/SensapiApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.tanis.sensapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
