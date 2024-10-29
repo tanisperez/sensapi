efba20304a584fd09bacfbc0c9ed95125471c999
Tanis Pérez Nartallo <tanisperez@gmail.com>
2024-10-29 20:29:03 +0100

    Add Mac equals and hashCode

--- src/main/java/codes/tanis/sensapi/sensor/domain/Mac.java
+++ src/main/java/codes/tanis/sensapi/sensor/domain/Mac.java
@@ -20,4 +20,18 @@ public final class Mac {
     public String address() {
         return address;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Mac mac = (Mac) o;
+        return address.equals(mac.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return address.hashCode();
+    }
 }
